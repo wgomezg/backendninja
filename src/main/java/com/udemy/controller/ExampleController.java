554d2f60261eff5f76dc3cb14a9482eb73b389fc
wgomez <wilson.gomez.consultant@sothebys.com>
2017-11-10 09:17:56 -0500

    insertando datos simples en plantillas

---
+++ src/main/java/com/udemy/controller/ExampleController.java
@@ -0,0 +1,28 @@
+package com.udemy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/example")
+public class ExampleController {
+	private static final String EXAMPLE_VIEW = "example";
+
+	//Primera forma
+	@GetMapping("/exampleString")
+	public String exampleString(Model model) {
+		model.addAttribute("name", "Wil");
+		return EXAMPLE_VIEW;
+	}
+	
+	//Segunda forma
+	@GetMapping("/exampleMAV")
+	public ModelAndView exampleMAV() {
+		ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
+		mav.addObject("name", "Pepe");
+		return mav;
+	}
+}
