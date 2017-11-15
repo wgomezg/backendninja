672ee52574fcac5bb4dbe2560dde5a40f4bbe8f1
wgomez <wilson.gomez.consultant@sothebys.com>
2017-11-15 18:52:11 -0500

    Recibiendo una petición GET

---
+++ src/main/java/com/udemy/controller/Example2Controller.java
@@ -0,0 +1,32 @@
+package com.udemy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/example2")
+public class Example2Controller {
+	
+	private final static String EXAMPLE2_VIEW = "example2";
+	
+	//localhost:8080/example2/request1?nm=WIL
+	@GetMapping("/request1")
+	public ModelAndView request1(@RequestParam(name="nm", required=false, defaultValue="John Doe") String name) {
+		ModelAndView mav = new ModelAndView(EXAMPLE2_VIEW);
+		mav.addObject("nm_in_model", name);
+		return mav;
+	}
+	
+	//localhost:8080/example2/request2/WIL
+	@GetMapping("/request2/{nm}")
+	public ModelAndView request2(@PathVariable("nm") String name ) {
+		ModelAndView mav = new ModelAndView(EXAMPLE2_VIEW);
+		mav.addObject("nm_in_model", name);
+		return mav;
+	}
+			
+}
