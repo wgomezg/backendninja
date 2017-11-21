cabd694307fb0e0469dd1cb2d8ef5042e9e4a6d9
wgomez <wilson.gomez.consultant@sothebys.com>
2017-11-21 16:32:47 -0500

    Nuestro primer componente

--- src/main/java/com/udemy/controller/ExampleController.java
+++ src/main/java/com/udemy/controller/ExampleController.java
@@ -3,22 +3,30 @@ package com.udemy.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.udemy.component.ExampleComponent;
 import com.udemy.model.Person;
 
 @Controller
 @RequestMapping("/example")
 public class ExampleController {
 	private static final String EXAMPLE_VIEW = "example";
+	
+	@Autowired
+	@Qualifier("exampleComponent")
+	private ExampleComponent exampleComponent;
 
 	//Primera forma
 	@GetMapping("/exampleString")
 	public String exampleString(Model model) {
+		exampleComponent.sayHello();
 		model.addAttribute("people", getPeople());
 		return EXAMPLE_VIEW;
 	}
