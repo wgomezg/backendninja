acd33ebb289093dfee6a3463d14e9745832739d0
wilson gomez <ing.wgomez@gmail.com>
2017-11-20 21:00:46 -0500

    add 404 page

--- src/main/java/com/udemy/controller/Example3Controller.java
+++ src/main/java/com/udemy/controller/Example3Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.udemy.model.Person;
 
@@ -16,6 +17,16 @@ public class Example3Controller {
 	
 	public static final String FORM_VIEW = "form";
 	public static final String RESULT_VIEW = "result";
+	
+//	@GetMapping("/")
+//	public String redirect() {
+//		return "redirect:/example3/showform";
+//	}
+	
+	@GetMapping("/")
+	public RedirectView redirect() {
+		return new RedirectView("/example3/showform");
+	}
 	
 	@GetMapping("/showform")
 	public String showForm(Model model) {
