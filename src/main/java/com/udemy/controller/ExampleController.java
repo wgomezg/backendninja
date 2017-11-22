a10e2eee4e308f2ea57de2d5c69e2434ea0106d8
wgomez <wilson.gomez.consultant@sothebys.com>
2017-11-22 15:45:14 -0500

    Nuestro primer servicio

--- src/main/java/com/udemy/controller/ExampleController.java
+++ src/main/java/com/udemy/controller/ExampleController.java
@@ -1,8 +1,5 @@
 package com.udemy.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -12,12 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.udemy.component.ExampleComponent;
-import com.udemy.model.Person;
+import com.udemy.service.ExampleService;
 
 @Controller
 @RequestMapping("/example")
 public class ExampleController {
 	private static final String EXAMPLE_VIEW = "example";
+	
+	@Autowired
+	@Qualifier("exampleService")
+	private ExampleService exampleService;
 	
 	@Autowired
 	@Qualifier("exampleComponent")
@@ -27,7 +28,7 @@ public class ExampleController {
 	@GetMapping("/exampleString")
 	public String exampleString(Model model) {
 		exampleComponent.sayHello();
-		model.addAttribute("people", getPeople());
+		model.addAttribute("people", exampleService.getListPeople());
 		return EXAMPLE_VIEW;
 	}
 	
@@ -35,16 +36,8 @@ public class ExampleController {
 	@GetMapping("/exampleMAV")
 	public ModelAndView exampleMAV() {
 		ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
-		mav.addObject("people", getPeople());
+		mav.addObject("people", exampleService.getListPeople());
 		return mav;
 	}
 	
-	private List<Person> getPeople(){
-		 List<Person> people = new ArrayList<>();
-		 people.add(new Person("Pepe", 23));
-		 people.add(new Person("Luis", 30));
-		 people.add(new Person("Maria", 25));
-		 people.add(new Person("Wil", 20));
-		 return people;
-	}
 }
